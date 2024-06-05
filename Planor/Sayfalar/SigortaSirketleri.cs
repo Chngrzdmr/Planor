3e53a94bcc1c2ab51f125db65cf3785067970090
Chngrzdmr <49045343+Chngrzdmr@users.noreply.github.com>
2024-06-05 13:19:23 +0300

    improve

--- Planor/Sayfalar/SigortaSirketleri.cs
+++ Planor/Sayfalar/SigortaSirketleri.cs
@@ -1,10 +1,9 @@
-﻿using Planor.Kalaslar;
+using Planor.Kalaslar;
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Windows.Forms;
-
 
 namespace Planor.Sayfalar
 {
@@ -15,82 +14,81 @@ namespace Planor.Sayfalar
         {
             InitializeComponent();
         }
-        private void ana_sirketleri_getir()
-        {
-            gn.grid_view_getir("id,adi from t_ana_sirketler order by adi asc", dgw_sigorta_sirketleri);
-        }
 
+        private void PopulateGridView(string query, DataGridView gridView)
+        {
+            try
+            {
+                gn.grid_view_getir(query, gridView);
+                gridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                gridView.BackgroundColor = this.BackColor;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
         private void SigortaSirketleri_Load(object sender, EventArgs e)
         {
-            //ekrana göre ölçüleri ayarlamak için
             this.Width = new SistemForm().screens[new SistemForm().ekranno].WorkingArea.Width - ((new SistemForm().screens[new SistemForm().ekranno].WorkingArea.Width) / 8);
             this.Height = new SistemForm().screens[new SistemForm().ekranno].WorkingArea.Height - 111 - new Yonetici().yoneticiMenuPNL.Height;
 
-            ana_sirketleri_getir();
-            gn.grid_view_getir(" id,adi from t_sirketler order by adi asc", dgw_sirket_listesi);
-            dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-            dgw_sigorta_sirketleri.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-            dgw_sigorta_sirketleri.BackgroundColor = this.BackColor;
-            dgw_sirket_listesi.BackgroundColor = this.BackColor;
+            string anaSirketlerQuery = "id, adi from t_ana_sirketler order by adi asc";
+            string sirketlerQuery = " id,adi from t_sirketler order by adi asc";
 
-
+            PopulateGridView(anaSirketlerQuery, dgw_sigorta_sirketleri);
+            PopulateGridView(sirketlerQuery, dgw_sirket_listesi);
         }
 
         private void BtnSirketEkle_Click(object sender, EventArgs e)
         {
+            if (dgw_sigorta_sirketleri.CurrentRow.Cells[0].Value == null)
+            {
+                MessageBox.Show("Şirket Seçiniz");
+                return;
+            }
+
             try
             {
-                if (dgw_sigorta_sirketleri.CurrentRow.Cells[0].Value == null)
-                {
-                    MessageBox.Show("Şirket Seçiniz");
-                }
-                else
-                {
-                    string deger = dgw_sigorta_sirketleri.CurrentRow.Cells[0].Value.ToString();
-                    sirketlere_ekle(deger);
+                string anaSirketId = dgw_sigorta_sirketleri.CurrentRow.Cells[0].Value.ToString();
+                string anaSirketAdi = gn.en_son_kaydi_getir("t_ana_sirketler", "adi", $"where id='{anaSirketId}'");
 
-                    dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                }
+                List<string> tabloAdlari = new List<string> { "adi" };
+                ArrayList veriler = new ArrayList { anaSirketAdi };
 
+                string sonuc = gn.db_kaydet(tabloAdlari, "t_sirketler", veriler);
+
+                if (sonuc == "islem_tamam")
+                {
+                    PopulateGridView(" id,adi from t_sirketler order by adi asc", dgw_sirket_listesi);
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.ToString());
-            }
-        }
-
-        private void sirketlere_ekle(string deger)
-        {
-            string adi = gn.en_son_kaydi_getir("t_ana_sirketler", "adi", "where id='" + deger + "'");
-
-            List<string> TabloAdlari = new List<string>();
-            TabloAdlari.Add("adi");
-
-            ArrayList veriler = new ArrayList();
-            veriler.Add(adi);
-
-            string sonuc = gn.db_kaydet(TabloAdlari, "t_sirketler", veriler);
-            if (sonuc == "islem_tamam")
-            {
-                gn.grid_view_getir(" id,adi from t_sirketler order by adi asc", dgw_sirket_listesi);
+                MessageBox.Show(ex.Message);
             }
         }
 
         private void BtnSirketSil_Click(object sender, EventArgs e)
         {
+            if (dgw_sirket_listesi.CurrentRow.Cells[0] == null)
+            {
+                return;
+            }
+
             DialogResult result = MessageBox.Show("Şirketi Silmek İstedinize Eminmisiniz?", "Sil", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
             if (result.Equals(DialogResult.OK))
             {
-                if (dgw_sirket_listesi.CurrentRow.Cells[0] != null)
+                try
                 {
-                    string sonuc = gn.db_sil("t_sirketler", dgw_sirket_listesi.CurrentRow.Cells[0].Value.ToString());
+                    string sirketId = dgw_sirket_listesi.CurrentRow.Cells[0].Value.ToString();
+                    string sonuc = gn.db_sil("t_sirketler", sirketId);
+
                     if (sonuc == "işlem tamamlandı")
                     {
-                        gn.grid_view_getir(" id,adi from t_sirketler", dgw_sirket_listesi);
-
-                        dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-
+                        PopulateGridView(" id,adi from t_sirketler", dgw_sirket_listesi);
                         MessageBox.Show("Şirket Silindi");
                     }
                     else
@@ -98,124 +96,58 @@ namespace Planor.Sayfalar
                         MessageBox.Show("Bir Hata Oluştu");
                     }
                 }
-            }
-            else
-            {
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
 
         private void dgw_sirket_listesi_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
-            SirketAdıDuzeltPNL.Visible = false;
-            SirketAdıDuzeltPNL.Refresh();
-
-            if (dgw_sirket_listesi.CurrentRow.Cells[0] != null)
+            if (dgw_sirket_listesi.CurrentRow.Cells[0] == null)
             {
-                string deger = dgw_sirket_listesi.CurrentRow.Cells[0].Value.ToString();
-
-                LblSirketID.Text = deger.ToString();
-                TxtSirketAdi.Text = gn.en_son_kaydi_getir("t_sirketler", "adi", "where id='" + LblSirketID.Text + "'");
-
-                SirketAdıDuzeltPNL.Visible = true;
-                SirketAdıDuzeltPNL.Refresh();
+                return;
             }
+
+            string sirketId = dgw_sirket_listesi.CurrentRow.Cells[0].Value.ToString();
+            string sirketAdi = gn.en_son_kaydi_getir("t_sirketler", "adi", $"where id='{sirketId}'");
+
+            LblSirketID.Text = sirketId;
+            TxtSirketAdi.Text = sirketAdi;
+
+            SirketAdıDuzeltPNL.Visible = true;
+            SirketAdıDuzeltPNL.Refresh();
         }
 
         private void BtnKaydet_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(TxtSirketAdi.Text))
+            {
+                return;
+            }
+
             DialogResult result = MessageBox.Show("Şirketi Düzenlemek İstediginize Eminmisiniz?", "Düzenle", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
 
             if (result.Equals(DialogResult.OK))
             {
-
-                List<string> TabloAdlari = new List<string>();
-                TabloAdlari.Add("adi");
-
-                ArrayList veriler = new ArrayList();
-                veriler.Add(TxtSirketAdi.Text);
-
-                string sonuc = gn.db_duzenle(TabloAdlari, "t_sirketler", veriler, "id", LblSirketID.Text);
-                if (sonuc == "islem_tamam")
+                try
                 {
-                    gn.grid_view_getir("id,adi from t_sirketler order by adi asc", dgw_sirket_listesi);
-                    dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                    //SirketAdıDuzeltPNL.Visible = false;
-                }
+                    List<string> tabloAdlari = new List<string> { "adi" };
+                    ArrayList veriler = new ArrayList { TxtSirketAdi.Text };
 
-            }
-        }
+                    string sonuc = gn.db_duzenle(tabloAdlari, "t_sirketler", veriler, "id", LblSirketID.Text);
 
-        private void BtnSirketEkle_Click_1(object sender, EventArgs e)
-        {
-            try
-            {
-                if (dgw_sigorta_sirketleri.CurrentRow.Cells[0].Value == null)
-                {
-                    MessageBox.Show("Şirket Seçiniz");
-                }
-                else
-                {
-                    string deger = dgw_sigorta_sirketleri.CurrentRow.Cells[0].Value.ToString();
-                    sirketlere_ekle(deger);
-
-                    dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                }
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-        }
-
-        private void BtnSirketSil_Click_1(object sender, EventArgs e)
-        {
-            DialogResult result = MessageBox.Show("Şirketi Silmek İstedinize Eminmisiniz?", "Sil", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-            if (result.Equals(DialogResult.OK))
-            {
-                if (dgw_sirket_listesi.CurrentRow.Cells[0] != null)
-                {
-                    string sonuc = gn.db_sil("t_sirketler", dgw_sirket_listesi.CurrentRow.Cells[0].Value.ToString());
-                    if (sonuc == "işlem tamamlandı")
+                    if (sonuc == "islem_tamam")
                     {
-                        gn.grid_view_getir(" id,adi from t_sirketler", dgw_sirket_listesi);
-
-                        dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-
-                        MessageBox.Show("Şirket Silindi");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Bir Hata Oluştu");
+                        PopulateGridView("id,adi from t_sirketler order by adi asc", dgw_sirket_listesi);
+                        SirketAdıDuzeltPNL.Visible = false;
                     }
                 }
-            }
-            else
-            {
-            }
-        }
-
-        private void BtnKaydet_Click_1(object sender, EventArgs e)
-        {
-            DialogResult result = MessageBox.Show("Şirketi Düzenlemek İstediginize Eminmisiniz?", "Düzenle", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-
-            if (result.Equals(DialogResult.OK))
-            {
-
-                List<string> TabloAdlari = new List<string>();
-                TabloAdlari.Add("adi");
-
-                ArrayList veriler = new ArrayList();
-                veriler.Add(TxtSirketAdi.Text);
-
-                string sonuc = gn.db_duzenle(TabloAdlari, "t_sirketler", veriler, "id", LblSirketID.Text);
-                if (sonuc == "islem_tamam")
+                catch (Exception ex)
                 {
-                    gn.grid_view_getir("id,adi from t_sirketler order by adi asc", dgw_sirket_listesi);
-                    dgw_sirket_listesi.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                    //SirketAdıDuzeltPNL.Visible = false;
+                    MessageBox.Show(ex.Message);
                 }
-
             }
         }
     }
