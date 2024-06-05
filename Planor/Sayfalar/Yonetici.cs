e081995c9ebcbc88bc1712b9975df3ebb3f685f4
Chngrzdmr <49045343+Chngrzdmr@users.noreply.github.com>
2024-06-05 13:19:35 +0300

    improve

--- Planor/Sayfalar/Yonetici.cs
+++ Planor/Sayfalar/Yonetici.cs
@@ -1,4 +1,4 @@
-﻿using Guna.UI2.WinForms;
+using Guna.UI2.WinForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,20 +13,32 @@ namespace Planor.Sayfalar
 {
     public partial class Yonetici : UserControl
     {
+        private const int PanelMargin = 8;
+
         public Yonetici()
         {
             InitializeComponent();
-
+            SizeChanged += Yonetici_SizeChanged;
         }
 
         private void Yonetici_Load(object sender, EventArgs e)
         {
-            this.Width = new SistemForm().screens[new SistemForm().ekranno].WorkingArea.Width - ((new SistemForm().screens[new SistemForm().ekranno].WorkingArea.Width) / 8);
-            this.Height = new SistemForm().PanelSlider.Height;
+            AdjustSize();
+            AddChildControls();
+            YoneticiMenuDegistir("SİGORTA ŞİRKETLERİ");
+        }
 
+        private void AdjustSize()
+        {
+            var screen = Screen.FromControl(this);
+            Width = screen.WorkingArea.Width - (screen.WorkingArea.Width / 8);
+            Height = new SistemForm().PanelSlider.Height;
+        }
+
+        private void AddChildControls()
+        {
             try
             {
-
                 yoneticiSliderPNL.Controls.Add(new SigortaSirketleri());
                 yoneticiSliderPNL.Controls.Add(new SubeAyarlari());
                 yoneticiSliderPNL.Controls.Add(new KullaniciYonetimi());
@@ -37,83 +49,49 @@ namespace Planor.Sayfalar
             }
             finally
             {
-                yoneticiSliderPNL.Controls.Find("SigortaSirketleri", false)[0].SendToBack();
-                yoneticiSliderPNL.Controls.Find("SubeAyarlari", false)[0].SendToBack();
-                yoneticiSliderPNL.Controls.Find("KullaniciYonetimi", false)[0].SendToBack();
-                //ayarlarSliderPNL.Controls.Find("AnaSayfa2", false)[0].SendToBack();
-                //ayarlarSliderPNL.Controls.Find("Hakkimizda2", false)[0].SendToBack();
+                foreach (Control control in yoneticiSliderPNL.Controls)
+                {
+                    control.SendToBack();
+                }
             }
-            YoneticiMenuDegistir("SİGORTA ŞİRKETLERİ");
-
-            //yeniolcekleme();
-
-            //GorunumDegistir();
-
-            //MessageBox.Show("Yonetici _Load Son işlem: " + new SistemForm().PanelSlider.Width.ToString());
         }
-
-
-
-
-
-
-
-
 
         private void YoneticiMenuDegistir(string MenuText)
         {
-            switch (MenuText)
-            {
-                case "SİGORTA ŞİRKETLERİ":
-                    yoneticiSliderPNL.Controls.Find("SigortaSirketleri", false)[0].BringToFront();
-                    break;
-                case "ŞUBE AYARLARI":
-                    yoneticiSliderPNL.Controls.Find("SubeAyarlari", false)[0].BringToFront();
-                    break;
-                case "KULLANICILAR":
-                    yoneticiSliderPNL.Controls.Find("KullaniciYonetimi", false)[0].BringToFront();
-                    break;
-                case "BU BOS CASE KALACAK":
-                    yoneticiSliderPNL.Controls.Find("Silme Kalsın", false)[0].BringToFront();
-                    break;
-            }
             foreach (Guna2GradientButton button in yoneticiMenuPNL.Controls.OfType<Guna2GradientButton>())
             {
-                if (button.Text == MenuText)
+                button.Checked = button.Text == MenuText;
+            }
+
+            foreach (Control control in yoneticiSliderPNL.Controls)
+            {
+                control.Visible = false;
+                if (control.GetType().Name == MenuText.ToLower())
                 {
-                    button.Checked = true;
+                    control.Visible = true;
+                    control.BringToFront();
+                    break;
                 }
-                else
-                {
-                    button.Checked = false;
-                }
-                Application.DoEvents();
             }
         }
-
 
         private void SigortaSirketleriBTN_Click(object sender, EventArgs e)
         {
-            Guna2GradientButton item = (sender as Guna2GradientButton);
-            YoneticiMenuDegistir(item.Text);
+            var button = sender as Guna2GradientButton;
+            YoneticiMenuDegistir(button.Text);
         }
-
-
 
         private void GorunumDegistir()
         {
-            int wdth = new SistemForm().Width;
-            
-            
-            
-            int hght = new SistemForm().Height;
+            var screen = Screen.FromControl(this);
+            int wdth = screen.WorkingArea.Width;
+            int hght = screen.WorkingArea.Height;
 
-            this.yoneticiMenuPNL.Size = new Size(wdth / 9, 697);
-            this.YoneticiBTN_1.Size = new Size((wdth / 9) - 6, hght / 10);
-            this.YoneticiBTN_2.Size = new Size((wdth / 9) - 6, (hght / 10));
-            this.YoneticiBTN_3.Size = new Size((wdth / 9) - 6, (hght / 10));
+            yoneticiMenuPNL.Size = new Size(wdth / 9, hght - PanelMargin);
+            YoneticiBTN_1.Size = new Size((wdth / 9) - PanelMargin, hght / 10);
+            YoneticiBTN_2.Size = new Size((wdth / 9) - PanelMargin, hght / 10);
+            YoneticiBTN_3.Size = new Size((wdth / 9) - PanelMargin, hght / 10);
         }
-
 
         private void yeniolcekleme()
         {
@@ -129,20 +107,16 @@ namespace Planor.Sayfalar
 
             foreach (Control control in this.Controls)
             {
-
                 control.Font = new Font(control.Font.FontFamily, control.Font.Size * ((size1 + size2) / 2));
-
                 control.Scale(scale);
-
-
             }
         }
 
-
         private void Yonetici_SizeChanged(object sender, EventArgs e)
         {
-            //MessageBox.Show(this.Size.ToString());
-            }
+            GorunumDegistir();
+            yeniolcekleme();
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
