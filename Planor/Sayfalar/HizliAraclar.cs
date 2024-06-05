3ead414db4b539cb7abd599921c4fea6f4d689c6
Chngrzdmr <49045343+Chngrzdmr@users.noreply.github.com>
2024-06-05 13:32:39 +0300

    comment

--- Planor/Sayfalar/HizliAraclar.cs
+++ Planor/Sayfalar/HizliAraclar.cs
@@ -1,20 +1,26 @@
+// HizliAraclar.cs
+// This is a C# implementation of a user control named HizliAraclar (Quick Tools) in a Windows Forms application.
+
 using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-using Planor.Kalaslar;
-using Planor.Sayfalar;
+using Planor.Kalaslar; // Namespace for custom classes
+using Planor.Sayfalar; // Namespace for this class
 using System.Globalization;
 using System.Linq;
 
 namespace Planor.Sayfalar
 {
+    // HizliAraclar class representing the Quick Tools user control
     public partial class HizliAraclar : UserControl
     {
+        // Instance variables
         SistemForm sistm;
         General gn;
 
+        // Constructor
         public HizliAraclar()
         {
             InitializeComponent();
@@ -22,30 +28,38 @@ namespace Planor.Sayfalar
             gn = new General();
         }
 
+        // Handler for the Load event of the form
         private void HizliAraclar_Load(object sender, EventArgs e)
         {
+            // Adjust the width of the form based on the screen size
             this.Width = sistm.screens[sistm.ekranno].WorkingArea.Width - (sistm.screens[sistm.ekranno].WorkingArea.Width / 8);
             this.Height = sistm.PanelSlider.Height;
         }
 
+        // Handler for the Click event of button1
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
+                // Show a balloon tip with a message
                 sistm.Trayyy1.ShowBalloonTip(1000, "Bekleyiniz...", "Yeniden deneme için 15 saniye daha bekleyiniz.", ToolTipIcon.Info);
             }
             catch (Exception ex)
             {
+                // Display an error message if there's an exception while showing the balloon tip
                 MessageBox.Show($"An error occurred while showing the balloon tip: {ex.Message}");
             }
         }
 
+        // Handler for the Click event of PanodanImageBTN
         private void PanodanImageBTN_Click(object sender, EventArgs e)
         {
+            // Check if there's an image in the clipboard
             if (Clipboard.ContainsImage())
             {
                 try
                 {
+                    // Get the image from the clipboard, save it to a temporary file, and display it in the PictureBox
                     var image = Clipboard.GetImage();
                     var tempFilePath = Path.Combine(Path.GetTempPath(), $"{DateTime.Now.ToFileTime()}_satis-sozlesmesi_img.png");
                     image.Save(tempFilePath, System.Drawing.Imaging.ImageFormat.Png);
@@ -54,17 +68,21 @@ namespace Planor.Sayfalar
                 }
                 catch (Exception ex)
                 {
+                    // Display an error message if there's an exception while saving the image
                     MessageBox.Show("An error occurred while saving the image: " + ex.Message);
                 }
             }
             else
             {
+                // Display a message if there's no image in the clipboard
                 MessageBox.Show("Lütfen ilk önce \"Satış Sözleşmesi\" 'ni Sağtuş ile Kopyala işlemi yapınız...");
             }
         }
 
+        // Handler for the Click event of SatistanIptaliBaslatBTN
         private void SatistanIptaliBaslatBTN_Click(object sender, EventArgs e)
         {
+            // Check if required fields are filled
             if (string.IsNullOrEmpty(txt_kayityolu.Text) ||
                 cmb_sirket.SelectedIndex == -1 ||
                 string.IsNullOrEmpty(txt_policeno.Text) ||
@@ -72,10 +90,12 @@ namespace Planor.Sayfalar
                 string.IsNullOrEmpty(txt_noter.Text) ||
                 string.IsNullOrEmpty(txt_YevmiyeNo.Text))
             {
+                // Display a message if required fields are not filled
                 MessageBox.Show("Lütfen önce bilgileri eksiksiz doldurunuz...");
                 return;
             }
 
+            // Concatenate the input values into a single string
             var uzuncumle = $"{txt_kayityolu.Text};" +
                             $"{cmb_sirket.SelectedItem};" +
                             $"{txt_policeno.Text};" +
@@ -83,10 +103,12 @@ namespace Planor.Sayfalar
                             $"{txt_noter.Text};" +
                             $"{txt_YevmiyeNo.Text};";
 
+            // Encrypt the concatenated string
             var sifrelicumle = gn._Encrypt(uzuncumle, true);
 
             try
             {
+                // Start the SatistaNiptaLRobotu.exe process with the encrypted string as an argument
                 var process = new Process
                 {
                     StartInfo = new ProcessStartInfo
@@ -100,40 +122,49 @@ namespace Planor.Sayfalar
             }
             catch (Exception ex)
             {
+                // Display an error message if there's an exception while starting the process
                 MessageBox.Show("An error occurred while starting the process: " + ex.Message);
             }
         }
 
+        // Handler for the Click event of button1 (duplicate)
         private void button1_Click_1(object sender, EventArgs e)
         {
+            // Add a new item to the cmb_sirket ComboBox
             cmb_sirket.Items.Add(new ComboBoxItem<string>("sirket_adi", default(string)));
         }
 
+        // ComboBoxItem class definition
         public class ComboBoxItem<T>
         {
+            // Properties
             public string Name { get; set; }
             public T Value { get; set; }
 
+            // Constructor
             public ComboBoxItem(string name, T value)
             {
                 Name = name;
                 Value = value;
             }
 
+            // Override ToString method to display the Name property
             public override string ToString()
             {
                 return Name;
             }
         }
 
+        // Handler for the TextChanged event of txtesas4
         private void txtesas4_TextChanged(object sender, EventArgs e)
         {
+            // Check if the input is a valid number and less than 7 characters long
             if (!string.IsNullOrEmpty(txtesas4.Text) && txtesas4.Text.Length < 7)
             {
                 if (double.TryParse(txtesas4.Text, NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out double nValue))
                 {
-                    txts4.Text = String.Format("{0:N0}", nValue);
-                    SetTextBoxValue(txts0, nValue * 3.00);
+                    // Set TextBox values based on calculations
+                    SetTextBoxValue(txts4, nValue * 3.00);
                     SetTextBoxValue(txts1, nValue * 2.35);
                     SetTextBoxValue(txts2, nValue * 1.90);
                     SetTextBoxValue(txts3, nValue * 1.45);
@@ -188,15 +219,18 @@ namespace Planor.Sayfalar
                 }
                 else
                 {
+                    // Clear all TextBoxes if the input is not a valid number
                     ClearAllTextboxes();
                 }
             }
             else
             {
+                // Clear all TextBoxes if the input is empty or has more than 6 characters
                 ClearAllTextboxes();
             }
         }
 
+        // Method for setting TextBox values
         private void SetTextBoxValue(TextBox textBox, double value)
         {
             if (textBox != null)
@@ -205,6 +239,7 @@ namespace Planor.Sayfalar
             }
         }
 
+        // Method for clearing all TextBoxes
         private void ClearAllTextboxes()
         {
             var textBoxes = this.Controls.OfType<TextBox>().ToList();
