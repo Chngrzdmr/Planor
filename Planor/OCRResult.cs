8b1269247717c67177eb811e2ef52d698a5a8093
Chngrzdmr <49045343+Chngrzdmr@users.noreply.github.com>
2024-06-05 13:19:07 +0300

    improve

--- Planor/OCRResult.cs
+++ Planor/OCRResult.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +6,60 @@ using System.Threading.Tasks;
 
 namespace Planor
 {
+    /// <summary>
+    /// Represents the root object of the OCR response.
+    /// </summary>
     public class Rootobject
     {
-        public Parsedresult[] ParsedResults { get; set; }
-        public int OCRExitCode { get; set; }
-        public bool IsErroredOnProcessing { get; set; }
-        public string ErrorMessage { get; set; }
-        public string ErrorDetails { get; set; }
+        /// <summary>
+        /// Gets or sets the parsed results.
+        /// </summary>
+        public public Parsedresult[] ParsedResults { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exit code of the OCR operation.
+        /// </summary>
+        public public int OCRExitCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether an error occurred during processing.
+        /// </summary>
+        public public bool IsErroredOnProcessing { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        public public string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error details.
+        /// </summary>
+        public public string ErrorDetails { get; set; }
     }
 
+    /// <summary>
+    /// Represents the parsed result of a single file.
+    /// </summary>
     public class Parsedresult
     {
-        public object FileParseExitCode { get; set; }
-        public string ParsedText { get; set; }
-        public string ErrorMessage { get; set; }
-        public string ErrorDetails { get; set; }
+        /// <summary>
+        /// Gets or sets the exit code of the file parse operation.
+        /// </summary>
+        public public object? FileParseExitCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parsed text.
+        /// </summary>
+        public public string ParsedText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        public public string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error details.
+        /// </summary>
+        public public string ErrorDetails { get; set; }
     }
 }
