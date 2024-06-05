3771a569766c60396df403b3ef415d7716da80b3
Chngrzdmr <49045343+Chngrzdmr@users.noreply.github.com>
2024-06-05 13:31:46 +0300

    comment

--- Planor/Properties/AssemblyInfo.cs
+++ Planor/Properties/AssemblyInfo.cs
@@ -1,35 +1,57 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+// This is a C# file that contains assembly attributes for a Planor application.
+// Assembly attributes are used to provide general information about a
+// assembly, such as its title, description, copyright, and version number.
 
-// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
-// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
-// bu öznitelik değerlerini değiştirin.
+using System.Reflection; // Needed for working with assembly attributes
+using System.Runtime.InteropServices; // Needed for COM interop services
+
+// The following assembly-level attributes provide general information about the
+// Planor application, such as its title, description, company name, product name,
+// copyright, and culture. These attributes can be viewed and modified as needed.
+
+// The AssemblyTitle attribute provides a title for the Planor application.
 [assembly: AssemblyTitle("Planor")]
+
+// The AssemblyDescription attribute provides a description for the Planor application.
 [assembly: AssemblyDescription("")]
+
+// The AssemblyConfiguration attribute provides additional configuration information
+// for the Planor application.
 [assembly: AssemblyConfiguration("")]
+
+// The AssemblyCompany attribute provides the name of the company that produced the
+// Planor application.
 [assembly: AssemblyCompany("CMSigorta")]
+
+// The AssemblyProduct attribute provides the name of the Planor application.
 [assembly: AssemblyProduct("Planor")]
+
+// The AssemblyCopyright attribute provides the copyright information for the Planor application.
 [assembly: AssemblyCopyright("Copyright ©  2021")]
+
+// The AssemblyTrademark attribute provides the trademark information for the Planor application.
 [assembly: AssemblyTrademark("")]
+
+// The AssemblyCulture attribute provides the culture information for the Planor application.
 [assembly: AssemblyCulture("")]
 
-// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
-// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
-// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+// The ComVisible attribute determines whether the types in the Planor application are
+// visible to COM components. By setting ComVisible to false, the types in the
+// Planor application are not visible to COM components.
 [assembly: ComVisible(false)]
 
-// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+// The Guid attribute provides a unique identifier for the Planor application.
+// This identifier is used when the Planor application is exposed to COM.
 [assembly: Guid("3abed9a9-569f-408e-b990-98b358b8ccad")]
 
-// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
-//
-//      Ana Sürüm
-//      İkincil Sürüm 
-//      Yapı Numarası
-//      Düzeltme
-//
-// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
-// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
-// [assembly: AssemblyVersion("1.0.*")]
+// The AssemblyVersion attribute provides the version number for the Planor application.
+// The version number consists of four parts: major version, minor version, build number,
+// and revision number. These parts can be specified explicitly or using wildcards.
+// In this example, all four parts are specified explicitly.
 [assembly: AssemblyVersion("1.0.0.0")]
+
+// The AssemblyFileVersion attribute provides the file version number for the Planor application.
+// The file version number consists of four parts: major version, minor version, build number,
+// and revision number. These parts can be specified explicitly or using wildcards.
+// In this example, all four parts are specified explicitly.
 [assembly: AssemblyFileVersion("1.0.0.0")]
