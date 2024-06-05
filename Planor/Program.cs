bc77267ef2dce670b10f60150823335067b09eb8
Chngrzdmr <49045343+Chngrzdmr@users.noreply.github.com>
2024-06-05 13:19:05 +0300

    improve

--- Planor/Program.cs
+++ Planor/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Planor
 {
@@ -12,20 +12,34 @@ namespace Planor
         /// <summary>
         /// Uygulamanın ana girdi noktası.
         /// </summary>
-        static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+        private static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         [STAThread]
         static void Main()
         {
-            if (mutex.WaitOne(TimeSpan.Zero, true))
+            try
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new LoginForm());
+                if (mutex.WaitOne(TimeSpan.Zero, true))
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new LoginForm());
+                }
+                else
+                {
+                    ShowWindow(GetConsoleWindow(), 0); // hide console window
+                    MessageBox.Show("Program sadece bir defa açılabilir.");
+                }
+
                 mutex.ReleaseMutex();
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Program sadece bir defa açılabilir.");
+                // handle any exceptions that might occur
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
     }
